cd37028eec44c7b04969e6fafa710a9977230c46
Prischep <ya.pryshchep@yandex.by>
2020-02-14 23:52:33 +0300

    Added unit for Travis CI using Headless Chrome

--- karma.conf.js
+++ karma.conf.js
@@ -27,6 +27,12 @@ module.exports = function (config) {
     autoWatch: true,
     browsers: ['Chrome'],
     singleRun: false,
+    customLaunchers: {
+      ChromeHeadlessNoSandbox: {
+        base: 'ChromeHeadless',
+        flags: ['--no-sandbox']
+      }
+    },
     restartOnFileChange: true
   });
 };
