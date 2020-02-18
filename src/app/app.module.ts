6c96f7876c654a7f99125e27dc53c0bade37748a
Prischep <ya.pryshchep@yandex.by>
2020-02-18 22:03:40 +0300

    Added main page layout

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,4 +1,5 @@
 import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
@@ -6,13 +7,47 @@ import { AppComponent } from './app.component';
 import { StoreModule } from '@ngrx/store';
 import { StoreDevtoolsModule } from '@ngrx/store-devtools';
 import { environment } from '../environments/environment';
+import {
+  NzButtonModule,
+  NzDatePickerModule, 
+  NZ_I18N, 
+  ru_RU, 
+  NzTimePickerModule, 
+  NzInputNumberModule, 
+  NzSelectModule,
+  NzMenuModule
+} from 'ng-zorro-antd';
+import { MainPageComponent } from './main-page/main-page.component';
+import { NzIconModule } from 'ng-zorro-antd/icon';
+import { registerLocaleData } from '@angular/common';
+import ru from '@angular/common/locales/ru';
+import { HttpClientModule } from '@angular/common/http';
+registerLocaleData(ru);
+import { IconDefinition } from '@ant-design/icons-angular';
+import { NZ_ICONS } from 'ng-zorro-antd';
+import * as AllIcons from '@ant-design/icons-angular/icons';
+
+const antDesignIcons = AllIcons as {
+  [key: string]: IconDefinition;
+};
+const icons: IconDefinition[] = Object.keys(antDesignIcons).map(key => antDesignIcons[key])
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MainPageComponent
   ],
   imports: [
     BrowserModule,
+    BrowserAnimationsModule,
+    NzButtonModule,
+    NzDatePickerModule,
+    NzTimePickerModule,
+    NzInputNumberModule,
+    NzIconModule,
+    NzSelectModule,
+    HttpClientModule,
+    NzMenuModule,
     AppRoutingModule,
     StoreModule.forRoot({}),
     StoreDevtoolsModule.instrument({
@@ -21,7 +56,7 @@ import { environment } from '../environments/environment';
       logOnly: environment.production
     })
   ],
-  providers: [],
+  providers: [{ provide: NZ_I18N, useValue: ru_RU }, { provide: NZ_ICONS, useValue: icons }],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
